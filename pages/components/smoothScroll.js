07b1fdad72a6be4be6e0f6e59c6d88c169f562ea
chitru <schitru@gmail.com>
2022-01-27 00:06:59 +0800

    doing nothing

---
+++ pages/components/smoothScroll.js
@@ -0,0 +1,40 @@
+import React,  {useEffect, useRef } from "react";
+import useWindowSize from "./useWindowSize";
+
+const SmoothScroll = ({children}) => {
+    const windowSize = useWindowSize();
+    const scrollRef = useRef();
+
+    const data = {
+        ease: 0.1,
+        current: 0,
+        previous: 0,
+        rounded: 0,
+    }
+
+    useEffect(() => {
+        setBodyHeight();
+    }, [windowSize.height]);
+
+    const setBodyHeight = () => {
+        document.body.style.height = `${scrollRef.current.getBoundingClientRect().height}px`;
+    }
+
+    const smoothScrollingHandler = () => {
+        data.current = window.scrollY;
+        data.previous += (data.current -data.previous) * data.ease;
+        data.rounded = Math.round(data.previous * 100) / 100;
+
+        scrollRef.current.style.transform = `translateY(-${data.previous}px)`;
+
+        requestAnimationFrame(() => smoothScrollingHandler());
+    }
+
+    return(
+        <div className="scroll-smooth">
+            <div ref={scrollRef}>{children}</div>
+        </div>
+    )
+}
+
+export default SmoothScroll;
